ffe6b0e70b854811a2a79ec4ac1dfbfe4a83edb3
Jed Peek <jedpeek@yahoo.com>
2018-07-02 21:48:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(name:name, breed:breed, id= nil)
+  def initialize(name:name, breed:breed, id:nil)
     @name = name
     @breed = breed
     @id = id
